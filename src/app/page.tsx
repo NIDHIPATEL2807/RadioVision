cc3ccecf69e81c115e04e2085ff355fbf811701d
Enayut <vishvignair@gmail.com>
2025-02-15 12:58:22 +0530

    Authentication Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { redirect } from 'next/navigation'
+import { auth } from '@clerk/nextjs/server'
 
-export default Home
+export default async function Home() {
+  const { userId } = await auth();
+  if(userId) {
+    redirect('/dashboard');
+  }
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="text-2xl">Home Page</div>
+    </div>
+  );
+}
